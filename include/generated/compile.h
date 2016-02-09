9b42139d4cb4d87fa288ac30a4c9a25a00f6f4d0
Ubuntu <assusdan@assusdan.assusdan.f6.internal.cloudapp.net>
2016-02-09 16:51:29 +0000

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 16 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#16 SMP PREEMPT Tue Feb 9 06:58:36 UTC 2016"
+#define LINUX_COMPILE_BY "assusdan"
+#define LINUX_COMPILE_HOST "SRT"
+#define LINUX_COMPILER "gcc version 4.9.2 20140811 (prerelease) (Linaro GCC 4.9-2014.08) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,3 @@
+#define UTS_RELEASE "3.10.54paid"
+#define BUILD_INFO ""
+#define BUILD_FINGERPRINT ""
